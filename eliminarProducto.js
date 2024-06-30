0a9506e9ecc8f98587e04bc73a3c68d6662eab69
CarlosFCabrales <160705939+CarlosFCabrales@users.noreply.github.com>
2024-06-30 04:14:20 -0600

    Update eliminarProducto.js
    version actualizada

--- eliminarProducto.js
+++ eliminarProducto.js
@@ -2,7 +2,7 @@ import { conexionAPI } from "./conexionAPI.js";
 
 async function borrarProducto(id){  
      
-    console.log(id)
+    /* console.log(id) */
 
     await conexionAPI.eliminarProducto(id);
     window.location.reload();
