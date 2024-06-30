3eeca66533f545d04d2c8306e906beffc905e024
CarlosFCabrales <160705939+CarlosFCabrales@users.noreply.github.com>
2024-06-30 04:15:25 -0600

    Update crearProducto.js
    version actualizada

--- crearProducto.js
+++ crearProducto.js
@@ -1,16 +1,20 @@
 import { conexionAPI } from "./conexionAPI.js";
 
 const formulario = document.querySelector("[data-formulario]");
+/* let numeroMaximo = 100;  */
 
 async function crearProducto(evento) {
 
     evento.preventDefault();
 
+    /* let num =  Math.floor(Math.random()*numeroMaximo)+1;
+    const id = num.toString();  */
+
     const fotito = document.querySelector("[data-fotito]").value;
     const descripcion = document.querySelector("[data-descripcion]").value;
     const precio = document.querySelector("[data-precio]").value;    
 
-    await conexionAPI.enviarProducto(fotito,descripcion,precio);
+    await conexionAPI.enviarProducto(/*id,*/fotito,descripcion,precio);
     
     window.location.href="index.html"; 
 
