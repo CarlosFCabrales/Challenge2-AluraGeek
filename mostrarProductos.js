7c683d91753daf359a50fe2c8fda37059765676f
CarlosFCabrales <160705939+CarlosFCabrales@users.noreply.github.com>
2024-06-30 04:13:00 -0600

    Update mostrarProductos.js
    version actualizada

--- mostrarProductos.js
+++ mostrarProductos.js
@@ -9,7 +9,7 @@ function crearCard(id,fotito,descripcion,precio) {
                     <img class="itemImagen" src="${fotito}">
                     <p class="itemDescripcion">"${descripcion}"</p>
                     <p class="itemPrecio">"${precio}"</p>                    
-                    <input class="itemBorrar" type="submit" onclick="borrarProducto(${id});" value="Eliminar">
+                    <input class="itemBorrar" type="submit" onclick="borrarProducto('${id}');" value="Eliminar">
                 </div>`;
 
     return producto;                    
