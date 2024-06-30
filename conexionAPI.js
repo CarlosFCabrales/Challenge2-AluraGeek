d6d84df38b923489f0b4a5a4acee2f9e84fa1ac1
CarlosFCabrales <160705939+CarlosFCabrales@users.noreply.github.com>
2024-06-30 04:15:55 -0600

    Update conexionAPI.js
    version actualizada

--- conexionAPI.js
+++ conexionAPI.js
@@ -7,11 +7,12 @@ async function listarProductos() {
 }
 
 
-async function enviarProducto(fotito,descripcion,precio){
+async function enviarProducto(/*id,*/fotito,descripcion,precio){
     const conexion = await fetch("http://localhost:3001/productos",{
         method:"POST",
         headers:{"Content-type":"application/json"},
         body:JSON.stringify({
+            /*id:id,*/
             fotito:fotito,
             descripcion:descripcion,
             precio:precio
@@ -27,7 +28,7 @@ async function enviarProducto(fotito,descripcion,precio){
 async function eliminarProducto(id) {
     const conexion = await fetch(`http://localhost:3001/productos/${id}`,{
         method:"DELETE",
-        headers:{"Content-type":"application/json"},
+        headers:{"Content-type":"application/json"},  
     })
 
     const conexionConvertida = conexion.json();
